7fff73b958b1715250fa18bd8ec1580623b2ba89
Alexander Kunitsa <28026247+aakunitsa@users.noreply.github.com>
2021-01-26 14:37:24 -0500

    fixed a minor issue in simulator.py

--- src/python/qecirq/simulator/simulator.py
+++ src/python/qecirq/simulator/simulator.py
@@ -1,7 +1,7 @@
 import numpy as np
 from itertools import cycle
 from openfermion.ops import QubitOperator, IsingOperator
-from openfermion.transforms import get_sparse_operator
+from openfermion import get_sparse_operator
 
 from zquantum.core.openfermion import expectation, change_operator_type
 from zquantum.core.interfaces.backend import QuantumSimulator
