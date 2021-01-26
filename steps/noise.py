78d546fc0e7775575deefc318d239789f620b9e4
Alexander Kunitsa <28026247+aakunitsa@users.noreply.github.com>
2021-01-26 13:45:37 -0500

    fixed the doc string for the step and renamed the input parameter

--- steps/noise.py
+++ steps/noise.py
@@ -1,12 +1,13 @@
 from zquantum.core.utils import get_func_from_specs, save_noise_model
 from typing import Dict
 
-def get_cirq_noise_model(specs: Dict):
+def get_cirq_noise_model(noise_specs: Dict):
     """ Creates a simple (pre-baked) Cirq noise model and saves it in an Orquestra compatible json
 
         Args:
             specs: dictonary containing the following keys:  "module_name", "function_name", and
-                noise parameters such as T1, T2 (just T for some cases) and t_gate
+                noise parameters such as T1, T2 (just T in some cases) and t_gate; all the times
+                should be specified in s.
     """
     noise_model_func = get_func_from_specs(specs)
     noise_model = noise_model_func(**specs)
