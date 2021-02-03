f9a9a690362b0419d62b98ff55a15985c5936e28
Amara Katabarwa <Henriamaa@gmail.com>
2021-02-03 11:10:19 -0500

    added tests for noise models

--- src/python/qecirq/noise/basic.py
+++ src/python/qecirq/noise/basic.py
@@ -2,7 +2,9 @@ from cirq import (depolarize, asymmetric_depolarize,
                   amplitude_damp, phase_damp)
 from cirq import to_json, read_json
 import numpy as np
-
+from typing import Dict, Union
+from zquantum.core.utils import save_noise_model
+import json
 
 def get_depolarizing_channel(T, t_gate=10e-9):
     """Get the depolarizing channel
@@ -11,6 +13,9 @@ def get_depolarizing_channel(T, t_gate=10e-9):
         T (float): Decoherence parameter (seconds)
 
     """
+    assert T > 0
+    assert t_gate > 0
+
     gamma = (1 - pow(np.e, - 1/T*t_gate))
     noise_model = depolarize(gamma)
     return noise_model
@@ -28,6 +33,9 @@ def get_asymmetric_depolarize(T_1, T_2, t_gate=10e-9):
         t_gate (float) : Discretized time step over which the relaxation occurs over (seconds)
     
     """
+    assert T_1 > 0
+    assert T_2 > 0
+    assert t_gate > 0
 
     px = 0.25*(1- pow(np.e, - t_gate/T_1))
     py = 0.25*(1- pow(np.e, - t_gate/T_1))
@@ -62,6 +70,9 @@ def get_amplitude_damping(T_1, t_gate=10e-9):
         t_gate (float) : Discretized time step over which the relaxation occurs over (seconds)
     
     """
+    assert T_1 > 0
+    assert t_gate > 0
+
     gamma = (1 - pow(np.e, - 1/T_1*t_gate))
     noise_model = amplitude_damp(gamma)
     return noise_model
@@ -74,22 +85,32 @@ def get_phase_damping(T_2, t_gate=10e-9):
         t_gate (float) : Discretized time step over which the relaxation occurs over (seconds)
 
     """
-
+    assert T_2 > 0
+    assert t_gate > 0
     gamma = (1 - pow(np.e, - 1/T_2*t_gate))
     noise_model = phase_damp(gamma)
     return noise_model
 
-
-def load_noise_model_from_json(serialized_model):
+{
+    # noise model info
+}
+def load_noise_model_from_json(serialized_model: Union[Dict, str]):
     """Loads a cirq noise model (version 2)
 
     Args:
-        serialized_model (string): json str representation of a cirq noise model
+        serialized_model: json str representation of a cirq noise model
 
     Return
         noise_model (cirq.NoiseModel)
 
     """
+    if isinstance(serialized_model, dict):
+        serialized_model = json.dumps(serialized_model)
 
     noise_model = read_json(json_text = serialized_model)
     return noise_model
+
+def save_cirq_noise_model(noise_model, filename):
+    noise_model_data = to_json(noise_model)
+    save_noise_model(noise_model_data, 'qecirq.noise', 'load_noise_model_from_json', filename)
+
