28c4cd533be900b0ed47aa792f21e859163a7d51
mstechly <michal.stechly@gmail.com>
2021-09-27 10:26:12 -0400

    Initial commit.
    Co-authored-by: Amara Katabarwa <Henriamaa@gmail.com>
    Co-authored-by: William Simon <william.andrew.simon@gmail.com>
    Co-authored-by: Konrad Jałowiecki <dexter2206@gmail.com>
    Co-authored-by: Alex Juda <alexander.juda@zapatacomputing.com>
    Co-authored-by: Alex Kunitsa <28026247+aakunitsa@users.noreply.github.com>
    Co-authored-by: Marta Mauri <marta.mauri14@gmail.com>
    Co-authored-by: Ahmed Darwish <exclass9.24@gmail.com>

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="qe-cirq",
+    use_scm_version=True,
+    author="Zapata Computing, Inc.",
+    author_email="info@zapatacomputing.com",
+    description="Cirq package for Orquestra.",
+    url="https://github.com/zapatacomputing/qe-cirq ",
+    packages=setuptools.find_packages(where="src/python"),
+    package_dir={"": "src/python"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    setup_requires=["setuptools_scm~=6.0"],
+    install_requires=[
+        "z-quantum-core",
+        "cirq>=0.9.1,<=0.10",
+    ],
+)
