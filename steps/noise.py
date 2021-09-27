4f59d683885f98ccdc98dc3330c4f4e2d4593013
mstechly <michal.stechly@gmail.com>
2021-09-27 10:26:12 -0400

    Initial commit.
    Co-authored-by: Amara Katabarwa <Henriamaa@gmail.com>
    Co-authored-by: William Simon <william.andrew.simon@gmail.com>
    Co-authored-by: Konrad Jałowiecki <dexter2206@gmail.com>
    Co-authored-by: Alex Juda <alexander.juda@zapatacomputing.com>
    Co-authored-by: Alex Kunitsa <28026247+aakunitsa@users.noreply.github.com>
    Co-authored-by: Marta Mauri <marta.mauri14@gmail.com>

---
+++ steps/noise.py
@@ -0,0 +1,28 @@
+from zquantum.core.utils import create_object, save_noise_model
+from zquantum.core.typing import Specs
+import yaml
+import cirq
+
+
+def get_cirq_noise_model(noise_model_func_specs: Specs, **kwargs):
+    """Creates a simple (pre-baked) Cirq noise model and saves it in an Orquestra compatible json
+
+    Args:
+        specs: dictonary containing the following keys:  "module_name", "function_name", and
+            noise parameters such as T1, T2 (just T in some cases) and t_gate; all the times
+            should be specified in s.
+    """
+    if isinstance(noise_model_func_specs, str):
+        noise_model_func_specs = yaml.load(
+            noise_model_func_specs, Loader=yaml.SafeLoader
+        )
+    noise_model_func = create_object(noise_model_func_specs)
+
+    noise_model = noise_model_func(**kwargs)
+    serialized_noise_model = cirq.to_json(noise_model)
+    save_noise_model(
+        serialized_noise_model,
+        "qecirq.noise",
+        "load_noise_model_from_json",
+        "noise-model.json",
+    )
